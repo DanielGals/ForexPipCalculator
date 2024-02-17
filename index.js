d3967827046aecbd7497ad71329a288a6c71c333
DanielGals <janjangallaron@gmail.com>
2024-02-17 21:45:25 +0800

    Import Project Initialization

---
+++ index.js
@@ -0,0 +1,58 @@
+const lastAmount = localStorage.getItem("balance");
+const lastRisk = localStorage.getItem("risk");
+
+document.getElementById("balance").value = lastAmount;
+document.getElementById("risk").value = lastRisk;
+
+var stoplosser = document.getElementById("stoploss");
+stoplosser.addEventListener('keyup',()=>{
+    let amount = document.getElementById("balance").value;
+    let risk = document.getElementById("risk").value;
+    let stoploss = document.getElementById("stoploss").value;
+
+    let pipSize;
+    
+    risk = risk / 100; 
+    risk = risk + amount;
+    pipSize = risk / stoploss;
+    pipSize = pipSize * (10000 / 1);
+    pipSize = pipSize / 10;
+
+    n = pipSize.toFixed(2);
+
+    document.getElementById("clipboardButton").innerText = n;
+});
+
+const clipboardButton = document.getElementById("clipboardButton");
+clipboardButton.addEventListener('click',()=>{
+    var amount = document.getElementById("balance").value;
+    var risk = document.getElementById("risk").value;
+    var stoploss = document.getElementById("stoploss").value;
+
+
+    localStorage.setItem("balance", amount);
+    console.log(amount + " " + risk);
+    localStorage.setItem("risk", risk);
+
+    var pipSize;
+    
+    risk = risk / 100; 
+    risk = risk + amount;
+    pipSize = risk / stoploss;
+    pipSize = pipSize * (10000 / 1);
+    pipSize = pipSize / 10;
+
+    n = pipSize.toFixed(2);
+
+    clipboard(n);
+}) 
+
+
+
+function clipboard(pipSize) {
+    navigator.clipboard.writeText(pipSize);
+    document.getElementById("clipboardButton").style = "background-color: #4BB543"
+    setTimeout(function(){
+        document.getElementById("clipboardButton").style = "background-color: #1DA1F2";
+    }, 500);
+  }
